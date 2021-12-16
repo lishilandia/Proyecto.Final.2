4c3950d026d5da68bfccc2bb99a81535f9b3cd3b
Lishilandia <lisette.pavez@gmail.com>
2021-12-16 10:52:06 -0300

    Comentarios mencionan ejemplos de como jquery puede reemplazar JS

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//JQuery, Tarea clase 11
+
 const tarjeta = document.querySelector("tarjeta")
 btnAbrirFormulario = document.querySelector("#btn-abrir-formulario");
 
@@ -8,3 +10,43 @@ tarjeta.addEventListener("click", () => {
 btnAbrirFormulario.addEventListener("click", () => {
   btnAbrirFormulario.classList.toogle("active");
 })
+
+/*$("#btn-abrir-formulario").click(function ()=> {
+  
+})
+
+$("#btn-abrir-formulario").on("mouseenter", function () {
+  console.log("mouse en el boton");
+}
+  
+ $(".tarjeta").on("click", function () {
+  console.log("click en el boton");
+} 
+  
+  */
+
+  
+
+
+
+
+
+
+
+//body-inputs
+
+/*$("body").prepend(`<input type="text"   class="inputsClass">
+                   <input type="number" class="inputsClass">
+                   
+
+$(".inputsClass").change(function (e) { 
+    console.log(e.target.value);
+   
+});/*
+
+
+/*CSS
+$("p").css("backgound-color", "red");
+$("p:last").css("backgound-color", "orange");
+$("a").css("backgound-color", "green");
+  */
