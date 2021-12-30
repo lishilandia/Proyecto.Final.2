ae4d1661669b668b04dbcb7a291a75497c380728
Lishilandia <lisette.pavez@gmail.com>
2021-12-29 22:19:37 -0300

    avances del proyecto final solo en html, css y js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //JQuery, Tarea clase 11
 
-const tarjeta = document.querySelector("tarjeta")
-btnAbrirFormulario = document.querySelector("#btn-abrir-formulario");
+const tarjeta = document.querySelector('tarjeta');
+btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
 
-tarjeta.addEventListener("click", () => {
-  tarjeta.classList.toogle("active");
+tarjeta.addEventListener('click', () => {
+  tarjeta.classList.toogle('active');
 });
 
-btnAbrirFormulario.addEventListener("click", () => {
-  btnAbrirFormulario.classList.toogle("active");
+btnAbrirFormulario.addEventListener('click', () => {
+  btnAbrirFormulario.classList.toogle('active');
 })
 
 /*$("#btn-abrir-formulario").click(function ()=> {
